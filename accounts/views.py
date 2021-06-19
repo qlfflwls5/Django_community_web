82bf18d03a92b2469c96fa666c398eab23e65ca0
qlfflwls5 <qlfflwls2@gmail.com>
2021-06-19 22:33:17 +0900

    feat: Add default profile image
    2021-06-19 프로필 이미지 미등록 시 기본 이미지 추가

--- accounts/views.py
+++ accounts/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.forms import AuthenticationForm, PasswordChangeForm
 from django.contrib.auth import login as auth_login, logout as auth_logout
-from django.contrib.auth import get_user_model
+from django.contrib.auth import get_user_model, update_session_auth_hash
 from django.contrib.auth.decorators import login_required
 from django.views.decorators.http import require_POST, require_safe, require_http_methods
 from .forms import CustomUserCreationForm, CustomUserChangeForm
@@ -17,6 +17,7 @@ def signup(request):
         return redirect('community:index')
     if request.method == 'POST':
         form = CustomUserCreationForm(request.POST, request.FILES)
+        print(request.FILES)
         if form.is_valid():
             user = form.save()
             auth_login(request, user, backend='django.contrib.auth.backends.ModelBackend')
